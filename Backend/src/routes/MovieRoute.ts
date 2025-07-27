fe2289900e8d6b9ae1713621b0261ac9c3fc1d82
Chaitanya-chute <chaitanyachute11@gmail.com>
2025-07-27 11:22:51 +0530

    Routes updated

--- Backend/src/routes/MovieRoute.ts
+++ Backend/src/routes/MovieRoute.ts
@@ -6,21 +6,25 @@ import cloudinary from "../libs/cloudinary";
 const Movierouter = express.Router();
 
 Movierouter.post("/upload" ,async(req,res)=>{
-    const {moviename , overview , poster_path ,backdrop_path , genres , release_date } = req.body;
+    const {moviename , overview , poster_path ,backdrop_path , genres , release_date , original_language , duration} = req.body;
 
     try {
         let poster_url ;
     let backdrop_url ;
 
     if(poster_path){
-        const uploadResult = await cloudinary.uploader.upload(poster_path)
+        const uploadResult = await cloudinary.uploader.upload(poster_path ,{
+            height:200,
+            width:200,
+            crop:"fill"
+        })
         poster_url = uploadResult.secure_url;
     }
 
     if(backdrop_path){
         const uploadResult = await cloudinary.uploader.upload(backdrop_path , {
-            height: 200, 
-            width: 200, 
+            height: 350, 
+            width: 300, 
             crop: "fill"
         })
         backdrop_url = uploadResult.secure_url;
@@ -32,7 +36,9 @@ Movierouter.post("/upload" ,async(req,res)=>{
         poster_path:poster_url,
         backdrop_path:backdrop_url,
         genres:genres,
-        release_date:release_date
+        release_date:release_date,
+        duration:duration,
+        original_language:original_language
     })
 
     res.json({
@@ -48,9 +54,9 @@ Movierouter.post("/upload" ,async(req,res)=>{
 })
 
 
-Movierouter.get("/Poster", async (req, res) => {
+Movierouter.get("/poster", async (req, res) => {
     try {
-        const backdrops = await movieModel.find({}, { backdrop_path: 1, _id: 0 });
+        const backdrops = await movieModel.find({}, { poster_path: 1, _id:1 ,duration:1,genres:1,release_date:1 });
         res.status(200).json(backdrops); 
     } catch (err) {
         console.error("There is error  in  /movies/Poster", err);
@@ -61,7 +67,7 @@ Movierouter.get("/Poster", async (req, res) => {
 
 Movierouter.get("/detail" ,async(req,res)=>{
     try {
-        const data = await movieModel.find({},{moviename:1 ,_id:0 ,genres:1,release_date :1,backdrop_path:1});
+        const data = await movieModel.find({_id:1,release_date:1,backdrop_path:1 , overview:1 ,moviename:1 , original_language:1 , genres:1});
         res.json(data)
     } catch (error) {
         res.json({
@@ -71,5 +77,13 @@ Movierouter.get("/detail" ,async(req,res)=>{
     }
 
 
+})
+
+Movierouter.get("/:id", async(req,res)=>{
+    const movieId = req.params.id;
+
+    const movie = await movieModel.findById(movieId);
+
+    res.json(movie)
 })
 export default Movierouter;
